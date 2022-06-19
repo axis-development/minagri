8f7420b5b068c0da309e601cd514fa94b2d3f213
7518774 <tahar.alkaissi@mc.be>
2022-06-19 21:59:34 +0200

    add

--- src/main/java/com/axis/caravela/commons/entity/AddressDTO.java
+++ src/main/java/com/axis/caravela/commons/entity/AddressDTO.java
@@ -4,12 +4,9 @@ package com.axis.caravela.commons.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.io.Serializable;
-
 @Getter
 @Setter
-public class AddressDTO implements Serializable {
+public class AddressDTO  {
 
     private Long id;
     private String streetName;
